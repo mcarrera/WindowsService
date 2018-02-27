51d87ce1b25cc4a2dc3e5891373b9bfb1bd010fd
Marco <marco@farreachinc.com>
2018-02-27 09:40:56 -0800

    log events

--- WindowsService1/Service1.cs
+++ WindowsService1/Service1.cs
@@ -12,17 +12,40 @@ namespace WindowsService1
 {
     public partial class Service1 : ServiceBase
     {
+        private int eventId = 1;
+
         public Service1()
         {
             InitializeComponent();
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("MySource")) 
+            {         
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MySource","MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+            // Set up a timer to trigger every minute.  
+            System.Timers.Timer timer = new System.Timers.Timer();  
+            timer.Interval = 60000; // 60 seconds  
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);  
+            timer.Start();  
         }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)  
+        {  
+            // TODO: Insert monitoring activities here.  
+            eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);  
+        }  
 
         protected override void OnStart(string[] args)
         {
+            eventLog1.WriteEntry("In OnStart");
         }
 
         protected override void OnStop()
         {
+            eventLog1.WriteEntry("In onStop.");
         }
     }
 }
