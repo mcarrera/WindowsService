a43bbf613081fed7185c353b30e57ccc3a94a949
Marco <marco@farreachinc.com>
2018-02-27 09:52:20 -0800

    Project installer

--- WindowsService1/Service1.cs
+++ WindowsService1/Service1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices; 
 
 namespace WindowsService1
 {
@@ -40,12 +41,50 @@ namespace WindowsService1
 
         protected override void OnStart(string[] args)
         {
+            // Update the service state to Start Pending.  
+            var serviceStatus = new ServiceStatus
+            {
+                dwCurrentState = ServiceState.SERVICE_START_PENDING,
+                dwWaitHint = 100000
+            };
+            
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);  
             eventLog1.WriteEntry("In OnStart");
+            
+            // Update the service state to Running.  
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;  
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);  
         }
 
         protected override void OnStop()
         {
             eventLog1.WriteEntry("In onStop.");
         }
+
+        [DllImport("advapi32.dll", SetLastError=true)]  
+        private static extern bool SetServiceStatus(IntPtr handle, ref ServiceStatus serviceStatus);  
     }
+
+    public enum ServiceState  
+    {  
+        SERVICE_STOPPED = 0x00000001,  
+        SERVICE_START_PENDING = 0x00000002,  
+        SERVICE_STOP_PENDING = 0x00000003,  
+        SERVICE_RUNNING = 0x00000004,  
+        SERVICE_CONTINUE_PENDING = 0x00000005,  
+        SERVICE_PAUSE_PENDING = 0x00000006,  
+        SERVICE_PAUSED = 0x00000007,  
+    }  
+
+    [StructLayout(LayoutKind.Sequential)]  
+    public struct ServiceStatus  
+    {  
+        public int dwServiceType;  
+        public ServiceState dwCurrentState;  
+        public int dwControlsAccepted;  
+        public int dwWin32ExitCode;  
+        public int dwServiceSpecificExitCode;  
+        public int dwCheckPoint;  
+        public int dwWaitHint;  
+    };  
 }
